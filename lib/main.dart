1fb4f5fc563b33c0a4d3acb0c25259f226a8dd44
Faris Zulfequar <zulfequf@shernet.sheridancollege.ca>
2026-01-22 20:54:27 -0500

    Changed the background color of the screen

--- lib/main.dart
+++ lib/main.dart
@@ -31,39 +31,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor : Color(0xFFDBC6EB),
       appBar: AppBar(
         leading: Icon(Icons.house, size: 25,),
         backgroundColor: Color(0xFFFFC0CB),
-        
         title: Text(widget.title),
       ),
       body: Center(
-        
         child: Column(
-          
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('0', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: null,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
